d4cb75895d85607c2ec2c2e19d1b39ed2e56250e
Candide-cup <mariecandidedjiwanou@gmail.com>
2025-03-17 13:37:18 +0100

    modification

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const menuOpenButton = document.querySelector("#menu-open-button");
 const menuCloseButton = document.querySelector("#menu-close-button");
 
-menuOpenButton.addEventListener("click", () => {
-    document.body.classList.toggle("show-mobile-menu");
+menuOpenButton.addEventListener("touchstart", () => {
+  document.body.classList.toggle("show-mobile-menu");
 });
-menuCloseButton.addEventListener("click", () => menuOpenButton.click());
-// menuOpenButton.addEventListener("click", () => menuOpenButton.click()); 
+menuCloseButton.addEventListener("touchstart", () => menuOpenButton.click());
+// menuOpenButton.addEventListener("click", () => menuOpenButton.click());
